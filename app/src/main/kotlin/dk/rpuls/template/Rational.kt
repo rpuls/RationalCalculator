284461ceb8fb59f327717cce2a1b7fed218fc402
TimmosQuadros <timm.rasmussen@gmail.com>
2017-04-17 14:57:06 +0200

    Tilføjede mulighed for at forkorte brøker vha. greatest common divisor Euclid algoritmen.

--- app/src/main/kotlin/dk/rpuls/template/Rational.kt
+++ app/src/main/kotlin/dk/rpuls/template/Rational.kt
@@ -9,32 +9,51 @@ data class Rational(val n: Int, val d: Int) {
         return "$n/$d"
     }
 
-    operator fun times(other: Rational) =
-            Rational(this.n * other.n, this.d * other.d)
+    operator fun times(other: Rational) :Rational {
+        val div = gcd(this.n * other.n,this.d * other.d)
+        return Rational((this.n * other.n)/div, (this.d * other.d)/div)
+    }
 
-    operator fun div(other: Rational) =
-            Rational(this.n * other.d, this.d * other.n)
+    operator fun div(other: Rational) :Rational {
+        val div = gcd(this.n * other.d,this.d * other.n)
+        return Rational((this.n * other.d)/div, (this.d * other.n)/div)
+    }
 
     operator fun plus(other: Rational) :Rational {
         if(this.d==other.d){
-            return Rational(this.n+other.n,this.d)
+            val div = gcd(this.n+other.n,this.d)
+            return Rational((this.n+other.n)/div,this.d/div)
         }else{
+            val div = gcd((this.n * other.d + other.n * this.d),(this.d * other.d))
             return Rational(
-                    this.n * other.d + other.n * this.d,
-                    this.d * other.d
+                    (this.n * other.d + other.n * this.d)/div,
+                    (this.d * other.d)/div
             )
         }
     }
 
     operator fun minus(other: Rational):Rational{
         if(this.d==other.d){
-            return Rational(this.n-other.n,this.d)
+            val div = gcd(this.n-other.n,this.d)
+            return Rational((this.n-other.n)/div,this.d/div)
         }else{
+            val div = gcd((this.n * other.d - other.n * this.d),(this.d * other.d))
             return Rational(
-                    this.n * other.d - other.n * this.d,
-                    this.d * other.d
+                    (this.n * other.d - other.n * this.d)/div,
+                    (this.d * other.d)/div
             )
         }
+    }
+
+    private fun gcd(a: Int, b: Int): Int {
+        var a = a
+        var b = b
+        while (b > 0) {
+            val temp = b
+            b = a % b // % is remainder
+            a = temp
+        }
+        return a
     }
 
 }
